2f3c9e9b71e6e6a6094a547380d6863aeec23453
kaweendras <salitha.wijerathna@gmail.com>
2024-11-09 22:45:51 +0530

    CHG | Util for alerts

---
+++ src/utils/alertUtil.jsx
@@ -0,0 +1,11 @@
+import Swal from 'sweetalert2';
+
+export const showAlert = ({ title, text, icon, confirmButtonText, timer }) => {
+  return Swal.fire({
+    title,
+    text,
+    icon,
+    confirmButtonText,
+    timer
+  });
+};
