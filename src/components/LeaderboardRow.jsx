c5e146cc92ba89c806f36ac6cbf903b7c322fc10
kaweendras <salitha.wijerathna@gmail.com>
2024-11-09 23:00:27 +0530

    FIX | Displaying Email when not logged

--- src/components/LeaderboardRow.jsx
+++ src/components/LeaderboardRow.jsx
@@ -1,40 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
-const LeaderboardRow = ({
-  place,
-  name,
-  points,
-  change,
-  email,
-  userId,  // Assume you have userId or any unique identifier
-}) => {
+const LeaderboardRow = ({ place, name, points, change, email, userId }) => {
+  const [token, setToken] = useState("");
 
-    const [token, setToken] = useState("");
-const [url, setUrl] = useState("#");
-
-useEffect(() => {
-  const sesson = sessionStorage.getItem('jwt');
-  if (sesson !== null && sesson !== undefined && sesson !== '') {
-    setToken(sesson);
-    setUrl(`/user`);  // Assuming userId is the unique identifier for each user
-  } else {
-    setToken('');
-    setUrl('#');  // Redirect to login page if no session found
-  }
-
-}, [token]);
+  useEffect(() => {
+    const sesson = sessionStorage.getItem("jwt");
+    if (sesson !== null && sesson !== undefined && sesson !== "") {
+      setToken(sesson);
+    } else {
+      setToken("");
+    }
+  }, [token]);
 
   return (
-    <Link to={`${url}/${email}`} className="w-full">
+    <Link to={token !== "" ? `/user/${email}` : "#"} className="w-full">
       <div className="flex items-center justify-between py-2 px-2 border-b border-gray-200 last:border-0">
         {/* Place */}
         <div className="w-1/12 text-center">
-          {change === 'up' ? (
+          {change === "up" ? (
             <span className="text-green-500">▲</span>
-          ) : change === 'down' ? (
+          ) : change === "down" ? (
             <span className="text-red-500">▼</span>
-          ) : null}{' '}
+          ) : null}{" "}
           {place}
         </div>
 
