ffe65414bf498854106b38e82a66384d2d8b185a
kaweendras <salitha.wijerathna@gmail.com>
2024-11-10 00:14:29 +0530

    CHG | BG color to white

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import Navgation from "./components/Navgation"
-import Leaderboard from "./components/Leaderboard"
-import UserPage from "./components/UserPage"
-import Login from "./components/Login"
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from "react";
+import Navgation from "./components/Navgation";
+import Leaderboard from "./components/Leaderboard";
+import UserPage from "./components/UserPage";
+import Login from "./components/Login";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
-    <div className="bg-gray-500 min-h-[100vh]">
-<Navgation />
-<Routes>
-<Route path="/" element={<Leaderboard />} />
-<Route path="/user/:userId" element={<UserPage />} />  
-<Route path="/login" element={<Login />} />  
-</Routes>
-</div>
-</BrowserRouter>
-  )
+      <div className="min-h-[100vh]">
+        <Navgation />
+        <Routes>
+          <Route path="/" element={<Leaderboard />} />
+          <Route path="/user/:userId" element={<UserPage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
